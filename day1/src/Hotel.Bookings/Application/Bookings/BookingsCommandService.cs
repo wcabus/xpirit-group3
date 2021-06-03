57d3e6252bc0d3b878c2e8618a68fc54212642a1
Alexey Zimarev <alex@zimarev.com>
2021-06-03 16:22:01 +0200

    Added a bit more

--- day1/src/Hotel.Bookings/Application/Bookings/BookingsCommandService.cs
+++ day1/src/Hotel.Bookings/Application/Bookings/BookingsCommandService.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Threading.Tasks;
 using CoreLib;
 using Hotel.Bookings.Domain;
 using Hotel.Bookings.Domain.Bookings;
@@ -8,16 +6,15 @@ namespace Hotel.Bookings.Application.Bookings {
     public class BookingsCommandService : CommandService<Booking, BookingId, BookingState> {
         public BookingsCommandService(IAggregateStore store, Services.IsRoomAvailable isRoomAvailable) : base(store) {
             OnNew<BookingCommands.BookRoom>(
-                async (booking, cmd) => {
-                    await booking.BookRoom(
-                        new BookingId(cmd.BookingId),
-                        cmd.GuestId,
-                        new RoomId(cmd.RoomId),
-                        new StayPeriod(cmd.CheckInDate, cmd.CheckOutDate),
-                        new Money(cmd.BookingPrice, cmd.Currency),
-                        isRoomAvailable
-                    );
-                });
+                (booking, cmd) => booking.BookRoom(
+                    new BookingId(cmd.BookingId),
+                    cmd.GuestId,
+                    new RoomId(cmd.RoomId),
+                    new StayPeriod(cmd.CheckInDate, cmd.CheckOutDate),
+                    new Money(cmd.BookingPrice, cmd.Currency),
+                    isRoomAvailable
+                )
+            );
         }
     }
 }
