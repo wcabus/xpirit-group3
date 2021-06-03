6ede4cf8641c89baa76bd903d4df8587310c4177
Alexey Zimarev <alex@zimarev.com>
2021-06-03 11:40:23 +0200

    Initial commit

---
+++ day1/src/Hotel.Bookings/Application/Bookings/BookingsCommandService.cs
@@ -0,0 +1,15 @@
+using CoreLib;
+using Hotel.Bookings.Domain;
+using Hotel.Bookings.Domain.Bookings;
+using static Hotel.Bookings.Domain.Services;
+
+namespace Hotel.Bookings.Application.Bookings {
+    public class BookingsCommandService : CommandService<Booking, BookingId, BookingState> {
+        public BookingsCommandService(IAggregateStore store) : base(store) {
+            OnNew<BookingCommands.BookRoom>(
+                (booking, cmd) => {
+                    
+                });
+        }
+    }
+}
